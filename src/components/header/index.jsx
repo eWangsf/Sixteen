5dd9800f7d2b3d463a4bdab076703a54b66639b8
eWangsf <1342832773@qq.com>
2017-12-27 18:27:11 +0800

    feat: header version1

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -15,27 +15,33 @@ class Header extends Component {
     const { prefix } = this.props;
 
     return <div className={`${prefix}header flex`}>
-      <div className="logo" onClick={this.backToIndex.bind(this)}>
-        <span>网易云音乐</span>
-      </div>
-      <div className="nav flex">
-        <div className="btns">
-          <span className="icon step back"></span>
-          <span className="icon step forward"></span>
+      <div className="left flex">
+
+        <div className="logo" onClick={this.backToIndex.bind(this)}>
+          <span>网易云音乐</span>
         </div>
-        <div className="search flex">
-          <div className="icon"></div>
-          <div className="search-container">
-            <input type="text" placeholder="搜索音乐，歌手，歌词，用户" />
+        <div className="nav flex">
+          <div className="btns flex">
+            <span className="icon step back"></span>
+            <span className="icon step forward"></span>
+          </div>
+          <div className="search flex">
+            <div className="icon"></div>
+            <div className="search-container">
+              <input type="text" placeholder="搜索音乐，歌手，歌词，用户" />
+            </div>
           </div>
         </div>
-      </div>
-      <div className="setting">
-        <div className="btns-container">
-          <span className="icon mail"></span>
-          <span className="icon setting"></span>
+        <div className="setting flex">
+          <div className="btns-container flex">
+            <span className="icon mail"></span>
+            <span className="icon set"></span>
+          </div>
         </div>
-        <div className="user-container">
+
+      </div>
+      <div className="right flex">
+        <div className="my-info flex">
           <div className="avatar">
           </div>
           <div className="more-info">
